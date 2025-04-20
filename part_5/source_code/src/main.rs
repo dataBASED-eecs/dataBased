af7ee2955283b866f894f6768b8b7cc6a42b2145
Manoj Turaga <manoj.turaga@ku.edu>
2025-04-20 16:51:17 -0500

    adding more entities

--- part_5/source_code/src/main.rs
+++ part_5/source_code/src/main.rs
@@ -3,9 +3,9 @@ use dotenvy::dotenv;
 use fake::faker::phone_number::raw::PhoneNumber;
 use fake::locales::EN;
 use fake::{
-    faker::chrono::raw::Date, faker::company::en::CompanyName, faker::internet::raw::FreeEmail,
-    faker::lorem::en::Sentence, faker::name::en::FirstName, faker::name::en::LastName,
-    faker::number::en::Digit, Fake,
+    faker::barcode::raw::Isbn10, faker::barcode::raw::Isbn13, faker::chrono::raw::Date,
+    faker::company::en::CompanyName, faker::internet::raw::FreeEmail, faker::lorem::en::Sentence,
+    faker::name::en::FirstName, faker::name::en::LastName, faker::number::en::Digit, Fake,
 };
 use rand::Rng;
 use sqlx::{mysql::MySqlPoolOptions, types::BigDecimal, MySql, Pool, Row};
@@ -78,6 +78,140 @@ async fn create_tables(pool: &Pool<MySql>) -> Result<(), sqlx::Error> {
     .execute(pool)
     .await?;
 
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS book 
+                (
+                ISBN VARCHAR(20) NOT NULL,
+                Title TEXT NOT NULL,
+                PRIMARY KEY (ISBN)
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS book_series 
+                (
+                ID INT NOT NULL AUTO_INCREMENT,
+                NAME TEXT NOT NULL,
+                PRIMARY KEY (ID)
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS director 
+                (
+                ID INT NOT NULL AUTO_INCREMENT,
+                First_Name TEXT NOT NULL,
+                Last_Name TEXT NOT NULL,
+                PRIMARY KEY (ID)
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS studio 
+                (
+                ID INT NOT NULL AUTO_INCREMENT,
+                Name TEXT NOT NULL,
+                PRIMARY KEY (ID)
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS movie 
+                (
+                ISAN VARCHAR(24) NOT NULL,
+                Title TEXT NOT NULL,
+                PRIMARY KEY (ISAN)
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS material 
+                (
+                ID INT NOT NULL AUTO_INCREMENT,
+                PRIMARY KEY (ID)
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS book_copy 
+                (
+                ID INT NOT NULL,
+                PRIMARY KEY (ID),
+                FOREIGN KEY (ID) REFERENCES material(ID) ON DELETE CASCADE
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS movie_copy 
+                (
+                ID INT NOT NULL,
+                PRIMARY KEY (ID),
+                FOREIGN KEY (ID) REFERENCES material(ID) ON DELETE CASCADE
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS room 
+                (
+                Number INT NOT NULL AUTO_INCREMENT,
+                Capacity INT NOT NULL,
+                PRIMARY KEY (Number)
+                );
+             "#,
+    )
+    .execute(pool)
+    .await?;
+
+    sqlx::query(
+        r#"
+             CREATE TABLE IF NOT EXISTS community_event 
+                (
+                ID INT NOT NULL AUTO_INCREMENT,
+                Start_Time DATETIME NOT NULL,
+                End_Time DATETIME NOT NULL,
+                Longitude FLOAT NOT NULL,
+                Latitude FLOAT NOT NULL,
+                Capacity INT NOT NULL,
+                PRIMARY KEY (Number)
+                );
+             "# ,
+    )
+    .execute(pool)
+    .await?;
+
     Ok(())
 }
 
@@ -122,8 +256,8 @@ async fn populate_tables(pool: &Pool<MySql>) -> Result<(), sqlx::Error> {
             let fake_email: String = FreeEmail(EN).fake();
             let fake_date: NaiveDate = Date(EN).fake();
 
-            let area_code: u16 = rng.gen_range(100..1000);  // (xxx)
-            let prefix: u16 = rng.gen_range(100..1000);    // xxx-
+            let area_code: u16 = rng.gen_range(100..1000); // (xxx)
+            let prefix: u16 = rng.gen_range(100..1000); // xxx-
             let line_number: u16 = rng.gen_range(1000..10000); // xxxx
 
             // Format the phone number
@@ -136,7 +270,7 @@ async fn populate_tables(pool: &Pool<MySql>) -> Result<(), sqlx::Error> {
                     (?, ?, ?, ?, ?)
                     "#,
             )
-            .bind( member.0 )
+            .bind(member.0)
             .bind(fake_salary)
             .bind(fake_phone_number)
             .bind(fake_date)
@@ -157,8 +291,8 @@ async fn populate_tables(pool: &Pool<MySql>) -> Result<(), sqlx::Error> {
                 (?, ?)
                 "#,
         )
-        .bind( fake_first_name )
-        .bind( fake_last_name )
+        .bind(fake_first_name)
+        .bind(fake_last_name)
         .execute(pool)
         .await?;
     }
@@ -173,7 +307,143 @@ async fn populate_tables(pool: &Pool<MySql>) -> Result<(), sqlx::Error> {
                 (?)
                 "#,
         )
-        .bind( fake_company_name )
+        .bind(fake_company_name)
+        .execute(pool)
+        .await?;
+    }
+
+    for _ in 1..100 {
+        let book_title: String = Sentence(1..20).fake();
+
+        let isbn_string: String = if rand::random() {
+            Isbn10(EN).fake()
+        } else {
+            Isbn13(EN).fake()
+        };
+
+        sqlx::query(
+            r#"
+                INSERT INTO book (ISBN, Title) 
+                VALUES
+                (?, ?)
+                "#,
+        )
+        .bind(isbn_string)
+        .bind(book_title)
+        .execute(pool)
+        .await?;
+    }
+
+    for _ in 1..20 {
+        let book_series_name: String = Sentence(1..20).fake();
+
+        sqlx::query(
+            r#"
+                INSERT INTO book_series (Name) 
+                VALUES
+                (?)
+                "#,
+        )
+        .bind(book_series_name)
+        .execute(pool)
+        .await?;
+    }
+
+    for _ in 1..50 {
+        let fake_first_name: String = FirstName().fake();
+        let fake_last_name: String = LastName().fake();
+
+        sqlx::query(
+            r#"
+                INSERT INTO director (First_Name, Last_name) 
+                VALUES
+                (?, ?)
+                "#,
+        )
+        .bind(fake_first_name)
+        .bind(fake_last_name)
+        .execute(pool)
+        .await?;
+    }
+
+    for _ in 1..25 {
+        let fake_company_name: String = CompanyName().fake();
+
+        sqlx::query(
+            r#"
+                INSERT INTO studio (Name) 
+                VALUES
+                (?)
+                "#,
+        )
+        .bind(fake_company_name)
+        .execute(pool)
+        .await?;
+    }
+
+    for _ in 1..100 {
+        let book_title: String = Sentence(1..20).fake();
+
+        let bytes: [u8; 12] = rand::random();
+        let isan: String = bytes.iter().map(|b| format!("{b:02x}")).collect();
+
+        sqlx::query(
+            r#"
+                INSERT INTO movie (ISAN, Title) 
+                VALUES
+                (?, ?)
+                "#,
+        )
+        .bind(isan)
+        .bind(book_title)
+        .execute(pool)
+        .await?;
+    }
+
+    for i in 1..1000 {
+        sqlx::query(
+            r#"
+                INSERT INTO material ()
+                VALUES
+                ()
+                "#,
+        )
+        .execute(pool)
+        .await?;
+
+        if rand::random() {
+            sqlx::query(
+                r#"
+                    INSERT INTO book_copy (ID)
+                    VALUES
+                    (?)
+                    "#,
+            )
+            .bind(i)
+            .execute(pool)
+            .await?;
+        } else {
+            sqlx::query(
+                r#"
+                    INSERT INTO movie_copy (ID)
+                    VALUES
+                    (?)
+                    "#,
+            )
+            .bind(i)
+            .execute(pool)
+            .await?;
+        }
+    }
+    for _ in 1..10 {
+        sqlx::query(
+            r#"
+                INSERT INTO room (Capacity)
+                VALUES
+                (?)
+                "#,
+        )
+        .bind(rng.gen_range(1..100))
         .execute(pool)
         .await?;
     }
@@ -211,11 +481,22 @@ async fn main() -> Result<(), sqlx::Error> {
         .connect(&database_url)
         .await?;
 
-    let rows = sqlx::query("SHOW TABLES;").fetch_all(&pool).await?;
-
-    for row in rows {
-        let table_name: String = row.get(0); // Replace with actual column name
-        let query = format!("DROP TABLE {};", table_name);
+    for row in [
+        "staff",
+        "member",
+        "book",
+        "book_series",
+        "publisher",
+        "author",
+        "director",
+        "studio",
+        "book_copy",
+        "movie_copy",
+        "material",
+        "room",
+        "community_event"
+    ] {
+        let query = format!("DROP TABLE IF EXISTS {};", row);
 
         sqlx::query(&query).execute(&pool).await?;
     }
